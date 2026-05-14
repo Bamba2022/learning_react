b9a53e9a7333289e7f8658ef78911a7d94fc96a9
Ahmadou Bamba Samb <akhmadoubambasamb@gmail.com>
2026-05-14 20:08:01 +0000

    Update build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  
+  // ← C'est cette ligne qui est importante pour GitHub Pages
+  base: '/learning_react/',
+});
